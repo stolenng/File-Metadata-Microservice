aeb8a52d3ff945271bbe4fb6d0221c59a3a56f4b
Georgy Glezer <stolenng@gmail.com>
2016-07-25 20:04:16 +0000

    minor fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,8 @@
 'use strict';
 
 var path = process.cwd();
-/*
-var imageAPI = require(path + '/app/controllers/imageAPI.server.js');
-var imageParser = require("../models/imageParser.js");
-var history = require("../models/history.js");
-var apicache = require('apicache').options({ debug: true }).middleware;
-var imgAPI = new imageAPI();
-*/
-
 var multer  = require('multer');
 var upload = multer();
-
-
-
 
 module.exports = function (app) {
 
@@ -22,8 +11,17 @@ module.exports = function (app) {
 	});
 	
 	app.post('/', upload.single('image'),function (req, res) {
-		var size = 0;
-		res.json({ size : req.file.size});
+		if(req.file){
+			res.json({ size : req.file.size});
+		}
+		else{
+			res.status(400);
+			res.send('None shall pass');
+		}
+	});
+	
+	app.get('*', function (req, res) {
+		res.sendFile(path + '/public/index.html');
 	});
 
 };
